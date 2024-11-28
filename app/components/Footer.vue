b99bef9b83ab4d085c4a769e36426823d4018610
PeWe79 <poer.one79@gmail.com>
2024-11-28 15:51:02 +0700

    Add github on footer.

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -7,9 +7,12 @@
       </NuxtLink>
       <p class="order-1 mt-4 text-sm text-gray-500 md:order-none md:ml-4 md:mt-0 md:border-l-2 md:border-gray-200 md:py-2 md:pl-4">
         © {{ currentYear }} Your Awesome Company —
-        <a href="https://twitter.com/KSalout" class="ml-1 text-gray-600" rel="noopener noreferrer" target="_blank">@KSalout</a>
+        <a href="https://github.com/PeWe79" class="ml-1 text-gray-600" rel="noopener noreferrer" target="_blank">PeWe79</a>
       </p>
       <span class="mt-4 inline-flex justify-center md:ml-auto md:mt-0 md:justify-start">
+        <a class="text-gray-500">
+          <Icon name="bxl:github" class="h-6 w-6 text-zinc-500" aria-hidden="true" />
+        </a>
         <a class="text-gray-500">
           <Icon name="bxl:facebook" class="h-6 w-6 text-zinc-500" aria-hidden="true" />
         </a>
